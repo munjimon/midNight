e42c6b0407ea17ccc3293c980fb931b19bfd4b75
munjimon <qkrrjs0411@naver.com>
2019-01-14 18:07:45 +0900

    테마 선택 시 우측 상단에 현재 테마아이콘 썸네일로 보이게 변경 , 아이콘 리스트에선 제거하도록 변경

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,10 +1,32 @@
 const changeTheme = e => {
+  let newTag = document.createElement("img")
   const fileName = e.target.classList[1]
+  const thumbnail = document.querySelector(".changer").children[0]
   const newLink = document.getElementsByTagName("link").item(1)
+  // Theme change(css)
   if (fileName === undefined) return false
   localStorage.setItem("theme", e)
   newLink.setAttribute("href", `/style/css/${fileName}.css`)
+  // thumbnail change(icon)
+  if (thumbnail.localName === "i") {
+    changeThumbnail(newTag, fileName)
+    thumbnail.parentNode.replaceChild(newTag, thumbnail)
+  } else {
+    changeThumbnail(thumbnail, fileName)
+  }
+  // selected theme icon in iconList element delete -> change to thumbnail
+  const icons = document.querySelector(".theme").children
+  for (let i = 0; i < icons.length; i++) {
+    icons[i].children[0].classList[1] === fileName
+      ? icons[i].classList.add("theme-item--close")
+      : icons[i].classList.remove("theme-item--close")
+  }
 }
 
 const toggleChanger = () =>
   document.querySelector(".theme").classList.toggle("theme--close")
+
+const changeThumbnail = (elName, fileName) => {
+  elName.setAttribute("class", `theme-icon ${fileName}`)
+  elName.setAttribute("src", `/img/icons/${fileName}.png`)
+}
