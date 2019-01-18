9edb071021175dea4e5d56773ec9bd1d98663a20
munjimon <qkrrjs0411@naver.com>
2019-01-18 18:15:51 +0900

    네이버 번역 api 추가

--- server.js
+++ server.js
@@ -2,6 +2,35 @@ const http = require("http")
 const express = require("express")
 const app = express()
 const router = require("./router/main")(app)
+const bodyParser = require("body-parser")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const client_id = "TmgHjujgU2SdjACt8yqH"
+const client_secret = "_A6BVUGB3W"
+app.post("/translate", (req, res) => {
+  console.log(req.body.query)
+  const api_url = "https://openapi.naver.com/v1/language/translate"
+  const request = require("request")
+  const options = {
+    url: api_url,
+    form: { source: "ko", target: "en", text: req.body.query },
+    headers: {
+      "X-Naver-Client-Id": client_id,
+      "X-Naver-Client-Secret": client_secret
+    }
+  }
+  request.post(options, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      console.log(response)
+      res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" })
+      res.end(body)
+    } else {
+      res.status(response.statusCode).end()
+      console.log("error = " + response.statusCode)
+    }
+  })
+})
 
 /* 서버가 읽을 수 있도록 HTML 의 위치를 정의 */
 app.set("views", __dirname + "/views")
